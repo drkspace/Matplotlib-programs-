0a6abec7429df1454ed4ffecbbb3068353b9d834
Daniel Kramer <drkspace@gmail.com>
2017-02-21 14:05:24 -0500

    fixed

--- mandelbrot2.py
+++ mandelbrot2.py
@@ -1,9 +1,10 @@
 import numpy as np
 from PIL import Image
+import time
 import math
-height = 100000
-width = 100000
-max_value= 2
+height = 5000
+width = 5000
+max_value= 1
 min_value= -2
 break_out=100
 data = np.zeros((height+1, width+1, 3), dtype=np.uint8)
@@ -13,12 +14,10 @@ def remap(val, low1, upper1, low2, upper2):
     new_range=upper2-low2
     range_ratio=float(new_range)/old_range
     val-=low1
-    NewValue=val*range_ratio+low2
-    return NewValue
+    return val*range_ratio+low2
 def magnitude(x,y):
-    c2 = x*x+y*y
-    c = math.sqrt(c2)
-    return c
+    return math.sqrt(x*x+y*y)
+start_time=time.time()
 for x in range(width+1):
     #print a
     for y in range(height+1):
@@ -43,9 +42,12 @@ for x in range(width+1):
         n=math.log(math.log(n+(1/math.log(2))))*255
         if n>254:
                 n=0
-        data[y,x]=[0,n,0]
+        data[y,x]=[0,n,100]
         #print n
-
+    if x != 0 and (float(x)*100/width)%1==0:
+        percent = x*100/float(width)
+        time_left = (time.time()-start_time)*100/percent 
+        print "{}% of the way done at {}\nEstimated compleation time {}\n---------------".format(int(percent),time.strftime("%H:%M:%S %m %d %Y", time.localtime()), time.strftime("%H:%M:%S %m %d %Y", time.localtime(time.time()+time_left)))
 img=Image.fromarray(data, 'RGB')
 img.save('mandelbrot2.png')
 img.show()
